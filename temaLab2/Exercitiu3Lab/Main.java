d0b795a4041e82daf44823fb80f12282ead83e20
Apetroaei Cezar <njroffic@gmail.com>
2023-03-06 14:51:52 +0200

    Exercitiu3Lab implemented

--- temaLab2/Exercitiu3Lab/Main.java
+++ temaLab2/Exercitiu3Lab/Main.java
@@ -6,20 +6,7 @@ import java.util.List;
 
 class Main{
 
-    private static List<Integer> convertToList(Value value) {
-        List<Integer> result = new ArrayList<>();
-        Value lengthValue = Value.asValue(value.getArraySize());
 
-        int length = lengthValue.asInt();
-
-        for (int i = 0; i < length; i++) {
-            Value item = value.getArrayElement(i);
-            int intValue = item.asInt();
-            result.add(intValue);
-        }
-
-        return result;
-    }
 
     private static List<Integer> generateRandomNumbers(){
         Context function = Context.newBuilder().allowAllAccess(true).build();
@@ -32,12 +19,58 @@ class Main{
                             for i in range(20):
                                 numbers.append(random.randint(1, 100))
                             return numbers
+                            
+                        generate_random_numbers()
                         """);
+        return getIntegers(function, result);
+    }
+    private static List<Integer> processList(List<Integer> list) {
+        Context context = Context.newBuilder().allowAllAccess(true).build();
 
-        return convertToList(result);
+        Value result = context.eval("R", """
+                  f <- function(list) {
+                  sorted <- sort(list)
+                  trimmed <- sorted[4:16]
+                  print(mean(trimmed)
+                }
+                """).execute(list);
+
+        return getIntegers(context, result);
     }
 
-    public static void main(String[] args){
+    private static List<Integer> getIntegers(Context context, Value value) {
+        List<Integer> result = new ArrayList<>();
 
+        for (int i = 0; i < value.getArraySize(); i++) {
+            Value item = value.getArrayElement(i);
+            int intValue = item.asInt();
+            result.add(intValue);
+        }
+
+        context.close();
+
+        return result;
+    }
+
+    private static void displayList(List<Integer> list) {
+        Context context = Context.newBuilder().allowAllAccess(true).build();
+        Value bindings = context.getBindings("js");
+
+        bindings.putMember("list", list);
+
+        context.eval("js", "function displayList(list) { console.log(list); }");
+        context.eval("js", "displayList(list);");
+    }
+    public static void main(String[] args){
+        Context polyglot =  Context.create();
+
+        List<Integer> list = generateRandomNumbers();
+        System.out.println("Before sorting and trimming");
+        displayList(list);
+
+        System.out.println("After sorting and trimming");
+        displayList(processList(list));
+
+        polyglot.close();
     }
 }
