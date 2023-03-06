5030886dde16a9e7d3e8169405916205a7f76ad8
Apetroaei Cezar <njroffic@gmail.com>
2023-03-07 00:26:16 +0200

    exercitiu 3 tema solved

--- temaLab2/Exercitiu3Tema/src/Main.java
+++ temaLab2/Exercitiu3Tema/src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+import org.graalvm.polyglot.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Numarul de aruncari ale monedei: ");
+        int n = input.nextInt();
+
+        System.out.print("Numarul de paturunjele dorite: ");
+        int x = input.nextInt();
+
+        if (x < 0) {
+            x = 0;
+        } else if (x > n) {
+            x = n;
+        }
+
+        Context context = Context.newBuilder().allowAllAccess(true).build();
+
+        context.eval("R", """
+                pbinom <- function(n, x) {
+                    p <- c()
+                    for (i in 0:x) {
+                        p[i+1] <- pbinom(i, n, 0.5)
+                    }
+                    return(p)
+                }
+                """);
+
+        Value pBinom = context.getPolyglotBindings().getMember("pbinom");
+        Value result = pBinom.execute(n, x);
+
+        double[] probabilities = result.as(double[].class);
+
+        System.out.println("Probabilitatea de a obtine cel mult " + x + " ori pajura din " + n + " aruncari:");
+
+        for (int i = 0; i <= x; i++) {
+            System.out.println(i + ": " + probabilities[i]);
+        }
+
+        context.close();
     }
 }
+
+
+
